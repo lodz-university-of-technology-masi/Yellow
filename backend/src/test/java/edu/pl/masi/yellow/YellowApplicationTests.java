af285fa049f0928ec505f75fb961df0608603af4
Łukasz Pierścieniewski <l.pierscieniewski@gmail.com>
2019-03-25 12:48:57 +0100

    Spring start [MAS19YE-1]

---
+++ backend/src/test/java/edu/pl/masi/yellow/YellowApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.pl.masi.yellow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YellowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
