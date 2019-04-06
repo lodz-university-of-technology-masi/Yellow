0e39828c6e91859e42e933951cbc20cc0f4110de
Urszula Krzeszewska <ulak.k@wp.pl>
2019-04-06 14:36:40 +0200

    [MAS19YE-21] Research dotyczacy JS do sprawdzenia metryk uzytecznosci - [MAS19YE-22] time logging using console.time() and console.timeEnd() - [MAS19YE-23] length logging between clicks - [MAS19YE-24] logging number of cliks All prototyped in simple js.

---
+++ metricsPrototype/game.js
@@ -0,0 +1,41 @@
+let clickCounter = 0;
+
+// [INFO] This function does not work properly with scrolling.
+// The position of the mouse is always taken relative to the upper left corner.
+function printMousePos(event) {
+  calculateLength(event.clientX, event.clientY);
+  clickCounter++;
+  document.body.textContent = 'X: ' + event.clientX + ', Y: ' + event.clientY +
+      ' length: ' + length + ' number of clicks: ' + clickCounter;
+}
+
+let length = 0;
+let lastX = 0;
+let lastY = 0;
+
+function calculateLength(x, y) {
+  length += Math.sqrt((x - lastX) * (x - lastX) + (y - lastY) * (y - lastY));
+  lastX = x;
+  lastY = y;
+}
+
+let status = false;
+
+// This function runs after press any key.
+// After press any key printMousePos function is run and time between keypresses
+// is measured.
+function checkKeys(e) {
+  if (status === false) {
+    console.log('Start collecting data');
+    console.time('dataCollectingTime');
+    document.addEventListener('click', printMousePos);
+    status = true;
+  } else {
+    console.log('End collecting data');
+    document.removeEventListener('click', printMousePos);
+    console.timeEnd('dataCollectingTime');
+    status = false;
+  }
+}
+
+document.addEventListener('keypress', checkKeys);
