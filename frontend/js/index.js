946c7d53ae5fabc595abfb045286c393f0cbb3aa
Piotr Szymczak <dogier140@poczta.fm>
2019-04-14 18:38:59 +0200

    Fix duplicate line [MAS19YE-32]

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", function (ev) {
     apiCommunicator = new ApiCommunicator();
     signInController = new SignInController(apiCommunicator);
     signUpController = new SignUpController(apiCommunicator);
-    signUpController = new SignUpController(apiCommunicator);
     testsController = new TestsController(apiCommunicator);
 
     viewController = new ViewController(
