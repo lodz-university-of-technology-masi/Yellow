c6c80a98754899bdff346755325eb44f2f6d1a1b
Łukasz Pierścieniewski <l.pierscieniewski@gmail.com>
2019-05-11 14:51:47 +0200

    More testing

--- backend/src/main/java/edu/pl/masi/yellow/controller/api/v1/manage/Redactors.java
+++ backend/src/main/java/edu/pl/masi/yellow/controller/api/v1/manage/Redactors.java
@@ -3,7 +3,6 @@ package edu.pl.masi.yellow.controller.api.v1.manage;
 import edu.pl.masi.yellow.entity.UserEntity;
 import edu.pl.masi.yellow.manager.UserManager;
 import edu.pl.masi.yellow.model.LoginToken;
-import edu.pl.masi.yellow.model.request.RedactorManageRequest;
 import edu.pl.masi.yellow.model.response.GenericResponse;
 import edu.pl.masi.yellow.utils.exceptions.ForbiddenException;
 import org.springframework.beans.factory.annotation.Autowired;
