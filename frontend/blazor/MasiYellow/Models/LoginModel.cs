de0e920d2720e7ffb3a6ebee5b4eae39730f4a26
Piotr Szymczak <dogier140@poczta.fm>
2019-05-27 20:18:58 +0200

    Add login, register and navigation

---
+++ frontend/blazor/MasiYellow/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasiYellow.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [StringLength(16, ErrorMessage = "Username needs to be 6-16 characters long", MinimumLength = 6)]
+        public string Username { get; set; } = "testtest";
+
+        [Required]
+        [PasswordPropertyText(true)]
+        [StringLength(16, ErrorMessage = "Password needs to be 6-16 characters long", MinimumLength = 6)]
+        public string Password { get; set; } = "lollol";
+    }
+}
--- frontend/blazor/MasiYellow/Startup.cs
+++ frontend/blazor/MasiYellow/Startup.cs
@@ -1,3 +1,8 @@
+using System.Net;
+using Blazor.Extensions.Logging;
+using Blazored.Toast;
+using Blazored.Toast.Configuration;
+using MasiYellow.Infrastructure;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +12,9 @@ namespace MasiYellow
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<AuthorizationManager>();
+            services.AddLogging(builder => builder.AddBrowserConsole());
+            services.AddBlazoredToast(options => options.Position = ToastPosition.BottomRight);
         }
 
         public void Configure(IComponentsApplicationBuilder app)
