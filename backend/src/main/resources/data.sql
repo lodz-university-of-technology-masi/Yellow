dd0461a6ead4888729fca2749cdda4f8681a191f
Łukasz Pierścieniewski <l.pierscieniewski@gmail.com>
2019-06-08 14:25:23 +0200

    Fixed database issue

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO user_data (id, user_pass, user_role, user_name) VALUES
-    (1, 'password', 'USER', 'testUser1'),
-    (2, 'password', 'USER', 'testUser2'),
-    (3, 'password', 'USER', 'testUser3'),
-    (4, 'password', 'REDACTOR', 'testUser4'),
-    (5, 'password', 'REDACTOR', 'testUser5'),
-    (6, 'password', 'REDACTOR', 'testUser6'),
-    (7, 'password', 'REDACTOR', 'testUser7'),
-    (8, 'password', 'MODERATOR', 'testUser8'),
-    (9, 'password', 'MODERATOR', 'testUser9');
+INSERT INTO user_data (user_pass, user_role, user_name) VALUES
+    ('password', 'USER', 'testUser1'),
+    ('password', 'USER', 'testUser2'),
+    ('password', 'USER', 'testUser3'),
+    ('password', 'REDACTOR', 'testUser4'),
+    ('password', 'REDACTOR', 'testUser5'),
+    ('password', 'REDACTOR', 'testUser6'),
+    ('password', 'REDACTOR', 'testUser7'),
+    ('password', 'MODERATOR', 'testUser8'),
+    ('password', 'MODERATOR', 'testUser9');
 
 
 INSERT INTO test_data (id, test_name, owner_id) VALUES
@@ -18,19 +18,19 @@ INSERT INTO test_data (id, test_name, owner_id) VALUES
     (5, 'Simple test #5', 7),
     (6, 'Simple test #6', 7);
 
-INSERT INTO question_data (id, question_desc, question_language,
+INSERT INTO question_data (question_desc, question_language,
     question_meta, question_number, question_type, test_id) VALUES
-    (1, 'Pytanie 1:', 'PL', '|', 1, 'OPEN', 1),
-    (2, 'Pytanie 2:', 'PL', '|', 2, 'OPEN', 2),
-    (3, 'Pytanie 1:', 'PL', '|', 1, 'OPEN', 1),
-    (4, 'Pytanie 2:', 'PL', '|', 2, 'OPEN', 2),
-    (5, 'Pytanie 3:', 'PL', '|', 3, 'OPEN', 3),
-    (6, 'Pytanie 4:', 'PL', '|', 4, 'OPEN', 4),
-    (7, 'Pytanie 1:', 'PL', '|', 1, 'OPEN', 1),
-    (8, 'Pytanie 2:', 'PL', '|', 2, 'OPEN', 2),
-    (9, 'Pytanie 3:', 'PL', '|', 3, 'OPEN', 3),
-    (10, 'Pytanie 4:', 'PL', '|', 4, 'OPEN', 4),
-    (11, 'Question 1:', 'EN', '|', 1, 'OPEN', 1),
-    (12, 'Question 2:', 'EN', '|', 2, 'OPEN', 2),
-    (13, 'Question 3:', 'EN', '|', 3, 'OPEN', 3),
-    (14, 'Question 4:', 'EN', '|', 4, 'OPEN', 4);
+    ('Pytanie 1:', 'PL', '|', 1, 'OPEN', 1),
+    ('Pytanie 2:', 'PL', '|', 2, 'OPEN', 1),
+    ('Pytanie 1:', 'PL', '|', 1, 'OPEN', 2),
+    ('Pytanie 2:', 'PL', '|', 2, 'OPEN', 2),
+    ('Pytanie 3:', 'PL', '|', 3, 'OPEN', 2),
+    ('Pytanie 4:', 'PL', '|', 4, 'OPEN', 2),
+    ('Pytanie 1:', 'PL', '|', 1, 'OPEN', 3),
+    ('Pytanie 2:', 'PL', '|', 2, 'OPEN', 3),
+    ('Pytanie 3:', 'PL', '|', 3, 'OPEN', 3),
+    ('Pytanie 4:', 'PL', '|', 4, 'OPEN', 3),
+    ('Question 1:', 'EN', '|', 1, 'OPEN', 4),
+    ('Question 2:', 'EN', '|', 2, 'OPEN', 4),
+    ('Question 3:', 'EN', '|', 3, 'OPEN', 4),
+    ('Question 4:', 'EN', '|', 4, 'OPEN', 4);
